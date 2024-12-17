85b58c29b7b4309db8bd25274c3bec1090c2cf9a
MadCat9958 <madcat9958@gmail.com>
2024-12-17 11:56:34 +0300

    feat: template for an ability to change 'metric system'

--- config.py
+++ config.py
@@ -6,3 +6,19 @@ load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
 owners_id = (727261363, )
+
+def get_fake_chat_info(
+    chat_id: int,
+    thing_name: str = "ум",
+    thing_metric: str = "IQ",
+    min_value: int = -5,
+    max_value: int = 10
+) -> dict:
+    return {
+        "chat_id": chat_id,
+        "thing_name": thing_name,
+        "thing_metric": thing_metric,
+        "min_value": min_value,
+        "max_value": max_value,
+        "fake": True
+    }
--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import traceback
 from aiogram import Bot, Dispatcher, F, exceptions, types
 from aiogram.filters import Command
 from aiogram.types import ContentType, LabeledPrice, PreCheckoutQuery
+from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 
-from config import bot_token, owners_id
+from config import bot_token, owners_id, get_fake_chat_info
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,31 +20,45 @@ dp = Dispatcher()
 db = sqlite3.connect("database.db", autocommit=True)
 db.row_factory = sqlite3.Row
 cur = db.cursor()
-ONE_WEEK_IN_SECONDS = 3600 * 24 * 7
 
 cur.execute(
     """CREATE TABLE IF NOT EXISTS user (
         chat_id INTEGER,
         user_id INTEGER,
-        length INTEGER,
-        next_dick INTEGER
-    )
-    """
+        value INTEGER,
+        cooldown_until INTEGER
+    );"""
+)
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS chat (
+        chat_id INTEGER UNIQUE NOT NULL,
+        thing_name TEXT NOT NULL DEFAULT("ум"),
+        thing_metric TEXT NOT NULL DEFAULT("IQ"),
+        min_value INTEGER NOT NULL DEFAULT(-5),
+        max_value INTEGER NOT NULL DEFAULT(10) 
+    );"""
 )
 cur.execute(
     """CREATE TABLE IF NOT EXISTS user_cache (
         user_id INTEGER UNIQUE NOT NULL,
         user_name TEXT
-    )
-    """
+    );"""
 )
 
 
 def get_top_users(chat_id: int):
     cur.execute(f"""SELECT * FROM user WHERE chat_id = {chat_id}""")
     return sorted(
-        [dict(row) for row in cur.fetchall()], key=lambda u: u["length"], reverse=True
+        [dict(row) for row in cur.fetchall()], key=lambda u: u["value"], reverse=True
     )
+
+
+@dp.my_chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+async def handle_group_join(event: types.ChatMemberUpdated):
+    if cur.execute("SELECT * FROM chat WHERE chat_id = ?", (event.chat.id,)).fetchone() is not None:
+        return
+    
+    cur.execute("INSERT INTO chat (chat_id) VALUES (?)", (event.chat.id, ))
 
 
 @dp.message(F.migrate_to_chat_id)
@@ -64,8 +79,8 @@ async def migrate_to_chat_id_handler(message: types.Message):
     )
 
 
-@dp.message(Command("dick"))
-async def dick(message: types.Message):
+@dp.message(Command("luck"))
+async def try_luck(message: types.Message):
     if message.chat.id == message.from_user.id:
         return await message.reply("Данная команда доступна только в группах с ботом.")
 
@@ -79,10 +94,14 @@ async def dick(message: types.Message):
         or {
             "chat_id": message.chat.id,
             "user_id": tg_user.id,
-            "length": 0,
-            "next_dick": 0,
+            "value": 0,
+            "cooldown_until": 0,
             "fake": True,
         }
+    )
+    chat_info = dict(
+        cur.execute("SELECT * FROM chat WHERE chat_id = ?", (message.chat.id,)).fetchone()
+        or get_fake_chat_info(message.chat.id, thing_name="писюн", thing_metric="см")
     )
     cur.execute(
         "SELECT * FROM user_cache WHERE user_id = ?", (tg_user.id,)
@@ -98,28 +117,28 @@ async def dick(message: types.Message):
             "UPDATE user_cache SET user_name = ? WHERE user_id = ?",
             (tg_user.full_name, tg_user.id)
         )
-    if user["next_dick"] > time.time():
-        remaining = user["next_dick"] - time.time()
+    if user["cooldown_until"] > time.time():
+        remaining = user["cooldown_until"] - time.time()
         return await message.reply(
             f"Ты уже играл!\nОжидай ещё {round(remaining // 3600)} часов, {round(remaining % 3600 // 60)} минут и {round(remaining % 3600 % 60)} секунд."
         )
     
-    amount = int(random.randint(-5, 10))
+    amount = int(random.randint(chat_info["min_value"], chat_info["max_value"]))
 
-    text = f"{tg_user.full_name}, твой писюн увеличился на {amount} см."
+    text = f"{tg_user.full_name}, твой {chat_info['thing_name']} увеличился на {amount} {chat_info['thing_value']}."
     if amount == 0:
-        text = f"{tg_user.full_name}, твой писюн не изменился."
+        text = f"{tg_user.full_name}, твой {chat_info['thing_name']} не изменился."
     if amount < 0:
-        text = f"{tg_user.full_name}, твой писюн сократился на {amount * -1} см."
+        text = f"{tg_user.full_name}, твой {chat_info['thing_name']} сократился на {amount * -1} {chat_info['thing_metric']}."
 
     if user.get("fake", False):
         cur.execute(
-            f"""INSERT INTO user (chat_id, user_id, length, next_dick) VALUES
+            f"""INSERT INTO user (chat_id, user_id, value, cooldown_until) VALUES
             ({message.chat.id}, {tg_user.id}, {amount}, {int(datetime.datetime.now().timestamp() + 3600 * 12)})"""
         )
     else:
         cur.execute(
-            f"""UPDATE user SET length = {user['length'] + amount}, next_dick = {int(datetime.datetime.now().timestamp() + 3600 * 12)} 
+            f"""UPDATE user SET value = {user['value'] + amount}, cooldown_until = {int(datetime.datetime.now().timestamp() + 3600 * 12)} 
             WHERE user_id = {tg_user.id} AND chat_id = {message.chat.id}"""
         )
 
@@ -129,9 +148,15 @@ async def dick(message: types.Message):
             break
 
     await message.reply(
-        f"{text}\nТеперь размер составляет {(user['length'] + amount):,} см.\n"
+        f"{text}\nТеперь размер составляет {(user['value'] + amount):,} {chat_info['thing_value']}.\n"
         f"Теперь ты занимаешь {count} место в топе.\nСледующая попытка через 12 часов."
     )
+    if chat_info.get("fake", False):
+        cur.execute(
+            """INSERT INTO chat (chat_id, thing_name, thing_metric, min_value, max_value)
+            VALUES (?, ?, ?, ?, ?)""",
+            (chat_info["chat_id"], chat_info["thing_name"], chat_info["thing_metric"], chat_info["min_value"], chat_info["max_value"])
+        )
 
 
 @dp.message(Command("info"))
@@ -151,15 +176,19 @@ async def info(message: types.Message):
     )
 
     db_user = cur.fetchone()
+    chat_info = dict(
+        cur.execute("SELECT * FROM chat WHERE chat_id = ?", (message.chat.id,)) or 
+        get_fake_chat_info(message.chat.id, "писюн", "см")
+    )
     if db_user is None:
         text = (
-            "Этот игрок ещё ни разу не играл, поэтому его волына равна 0 см. Скажи ему использовать /dick для игры."
+            f"Этот игрок ещё ни разу не играл, поэтому его {chat_info['thing_name']} равна 0 {chat_info['thing_metric']}. Скажи ему использовать /luck для игры."
             if message.reply_to_message
-            else "Ты ещё ни разу не играл, поэтому твоя волына равна 0 см. Используй /dick для игры."
+            else f"Ты ещё ни разу не играл, поэтому твой {chat_info['thing_name']} равна 0 {chat_info['thing_metric']}. Используй /luck для игры."
         )
         return await message.reply(text)
 
-    remaining = (db_user["next_dick"] or 0) - round(time.time())
+    remaining = (db_user["cooldown_until"] or 0) - round(time.time())
     remaining_text = f"через {round(remaining // 3600)} часов, {round(remaining % 3600 // 60)} минут и {round(remaining % 3600 % 60)} секунд."
     if remaining <= 0:
         remaining_text = "сейчас!"
@@ -170,14 +199,14 @@ async def info(message: types.Message):
             break
 
     thing_name = (
-        "твой писюн эквивалентен"
-        if db_user["length"] >= 0
-        else "в твою пизду поместится"
+        "в твою пизду поместится"
+        if db_user["value"] <= 0 and chat_info["thing_name"] == "писюн" 
+        else f"твой {chat_info['thing_name']} эквивалентен"
     )
     text = (
-        f"{user.full_name}, {thing_name} {abs(db_user['length']):,} см.\n"
+        f"{user.full_name}, {thing_name} {(abs(db_user['value']) if chat_info['thing_name'] == 'писюн' else db_user['value']):,} {chat_info['thing_metric']}.\n"
         f"Ты занимаешь {count} место в топе.\n"
-        f"Следующий /dick: {remaining_text}"
+        f"Следующий /luck: {remaining_text}"
     )
 
     await message.reply(text)
@@ -195,16 +224,26 @@ async def info(message: types.Message):
             "UPDATE user_cache SET user_name = ? WHERE user_id = ?",
             (user.full_name, user.id)
         )
+    if chat_info.get("fake", False):
+        cur.execute(
+            """INSERT INTO chat (chat_id, thing_name, thing_metric, min_value, max_value)
+            VALUES (?, ?, ?, ?, ?)""",
+            (chat_info["chat_id"], chat_info["thing_name"], chat_info["thing_metric"], chat_info["min_value"], chat_info["max_value"])
+        )
 
 
-@dp.message(Command("top", "top_dick"))
+@dp.message(Command("top"))
 async def top(message: types.Message):
     if message.chat.id == message.from_user.id:
         return await message.reply("Данная команда доступна только в группах с ботом.")
 
     users = get_top_users(message.chat.id)
+    chat_info = dict(
+        cur.execute("SELECT * FROM chat WHERE chat_id = ?", (message.chat.id,)) or 
+        get_fake_chat_info(message.chat.id, "писюн", "см")
+    )
 
-    text = "Топ пипис:\n\n"
+    text = "Топ этого чата:\n\n"
     count = 0
     for count, user in enumerate(users, start=1):
         user_name = None
@@ -219,7 +258,7 @@ async def top(message: types.Message):
                 user_name = (await bot.get_chat(user["user_id"])).full_name
             except exceptions.TelegramBadRequest:
                 traceback.print_exc()
-        text += f"{count}. {user_name or 'Неизвестный'}: {user['length']:,} см.\n"
+        text += f"{count}. {user_name or 'Неизвестный'}: {user['value']:,} {chat_info["thing_metric"]}.\n"
         if user_cache is None and user_name is not None:
             cur.execute(
                 "INSERT INTO user_cache (user_id, user_name) VALUES (?, ?)", 
@@ -227,6 +266,13 @@ async def top(message: types.Message):
             )
 
     await message.reply(text)
+
+    if chat_info.get("fake", False):
+        cur.execute(
+            """INSERT INTO chat (chat_id, thing_name, thing_metric, min_value, max_value)
+            VALUES (?, ?, ?, ?, ?)""",
+            (chat_info["chat_id"], chat_info["thing_name"], chat_info["thing_metric"], chat_info["min_value"], chat_info["max_value"])
+        )
 
 
 @dp.message(Command("giveuserlink"))
@@ -248,7 +294,7 @@ async def giveuserlink(message: types.Message):
 async def editsize(message: types.Message):
     args = message.text.split(" ")
     if len(args) < 2:
-        return await message.reply("Введи, на сколько см изменить")
+        return await message.reply("Введи, на сколько изменить")
 
     if message.reply_to_message is None and len(args) < 3:
         return await message.reply(
@@ -274,17 +320,17 @@ async def editsize(message: types.Message):
     )
     if cur.fetchone() is None:
         cur.execute(
-            "INSERT INTO user (chat_id, user_id, length) VALUES (?, ?, ?)",
+            "INSERT INTO user (chat_id, user_id, value) VALUES (?, ?, ?)",
             (message.chat.id, user, int(args[1])),
         )
     else:
         cur.execute(
-            "UPDATE user SET length = length + ? WHERE chat_id = ? AND user_id = ?",
+            "UPDATE user SET value = value + ? WHERE chat_id = ? AND user_id = ?",
             (args[1], message.chat.id, user),
         )
 
     await message.reply(
-        f"Размер пиписьки изменён у {'пользователя с ID ' + str(user) if message.reply_to_message is None else (
+        f"Размер изменён у {'пользователя с ID ' + str(user) if message.reply_to_message is None else (
             message.reply_to_message.sender_chat or message.reply_to_message.from_user
         ).full_name}"
     )
@@ -297,7 +343,7 @@ async def buy_kd_reset(message: types.Message):
 
     await message.reply_invoice(
         title="Сброс кд",
-        description="Сброс срока /dick. Если КД прошло - звёзды улетят впустую!!!!",
+        description="Сброс срока /luck. Если КД прошло - звёзды улетят впустую!!!!",
         payload=f"KD_RESET_{(message.sender_chat or message.from_user).id}",
         provider_token=None,
         is_flexible=False,
@@ -324,7 +370,7 @@ async def handle_pre_checkout(query: PreCheckoutQuery):
 )
 async def on_success(message: types.Message):
     cur.execute(
-        "UPDATE user SET next_dick = 0 WHERE user_id = ?",
+        "UPDATE user SET cooldown_until = 0 WHERE user_id = ?",
         (int(message.successful_payment.invoice_payload.removeprefix("KD_RESET_")),),
     )
 
@@ -332,7 +378,7 @@ async def on_success(message: types.Message):
     await bot.send_message(
         owners_id[0],
         f"Пользователь с ID {message.from_user.id} ({message.from_user.full_name}) "
-        f"оплатил снятие КД на /dick для пользователя/канала с ID {message.successful_payment.invoice_payload.removeprefix("KD_RESET_")}",
+        f"оплатил снятие КД на /luck для пользователя/канала с ID {message.successful_payment.invoice_payload.removeprefix("KD_RESET_")}",
     )
 
 
